8d7f71a4b059bb2cae66ae8dd30a3cc738eda93c
nkernis <nkernis@gmail.com>
2017-05-31 23:12:27 -0400

    Task views dooooone

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -46,7 +46,8 @@ class EventsController < ApplicationController
   end
 
   def destroy
-    @event.delete
+    @event = Event.find(params[:id])
+    @event.destroy
     flash[:success] = "gewd jerb!"
     redirect_to root_path
   end
