82eeea44468e7664dbaa1a0ff690365544156d20
nkernis <nkernis@gmail.com>
2017-06-01 00:26:28 -0400

    fixing login issue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if @user = User.find_by(email: params[:email]) || @user.authenticate(params[:password])
+    if @user = User.find_by(email: params[:email]) && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
