73787ed9946b1feca97bd92075f67111285b8534
tkoar <terrancekoar@gmail.com>
2017-06-01 14:41:16 -0400

    made some stuff ~pretty~

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
+  get '/signout', to: 'sessions#destroy'
   delete '/signout', to: 'sessions#destroy'
 
 end
