fe440c22039719a0dcefc91beee3ab44d427385a
nkernis <nkernis@gmail.com>
2017-06-02 13:43:06 -0400

    ok

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   before_action :find_event, only: [:show, :edit, :update, :destroy]
 
   def index
-    @events = Event.all
+    @events = Event.order("start_date")
   end
 
   def new
