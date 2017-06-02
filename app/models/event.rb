577800fe0231ad0838fd5b5785fc31ec0633f0c7
tkoar <terrancekoar@gmail.com>
2017-06-02 14:08:42 -0400

    fixed dem flashes

--- app/models/event.rb
+++ app/models/event.rb
@@ -15,6 +15,8 @@ class Event < ApplicationRecord
   has_and_belongs_to_many :users
   has_many :tasks
   has_many :locations
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 
   def start_time
     self.start_date
